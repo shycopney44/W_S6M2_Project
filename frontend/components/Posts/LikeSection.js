692cb67e4543f5db98fc37f8efe0386532fe5516
Shy Copney <shydashacopney@gmail.com>
2024-10-15 04:55:55 -0400

    complete project

--- frontend/components/Posts/LikeSection.js
+++ frontend/components/Posts/LikeSection.js
@@ -14,7 +14,7 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div onClick={likePost} className='like-section-wrapper'>
           ❤️
         </div>
         <div className='like-section-wrapper'>
